2d03f963f5377714b51d213589ce7cd0bf7e5503
Hemashruthi <hemashruthi1220@gmail.com>
2024-08-20 15:58:25 +0530

    init

---
+++ demo.py
@@ -0,0 +1,39 @@
+import cv2
+import pickle
+import face_recognition
+import numpy as np
+
+#  Load the encoding file
+print("Loading Encode File___")
+file = open("SaveFile.p",'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded___")
+matchIndex = []
+img = cv2.imread(r"D:\Higher studies\Projects\fr_demo\Images\tv.jpeg")
+# img = cv2.imread(r"D:\downloads\12.png")
+# imgS = cv2.resize(img,(0,0),None,0.25, 0.25)
+imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+faceCurFrame = face_recognition.face_locations(imgS)
+encodeCurFrame = face_recognition.face_encodings(imgS,faceCurFrame)
+print(encodeCurFrame)
+
+for encodeFace , faceLoc in zip(encodeCurFrame,faceCurFrame):
+    matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+    faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+    # print("matches",matches)
+    # print("faceDis",faceDis)
+    matchIndex = np.argmin(faceDis)
+    print("Match Index", matchIndex)
+
+    if matches[matchIndex]:
+        print("Known Face Detected")
+        print("Match Index", matchIndex)
+        print(studentIds[matchIndex])
+    else:
+        print("Unknown face detected")
+
+cv2.waitKey(0)
---
+++ encodegenerator.py
@@ -0,0 +1,36 @@
+import cv2
+import face_recognition
+
+import pickle
+import os
+# Importing student image
+
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+print(pathList)
+imgList = []
+studentIds = []
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath,path)))
+    studentIds.append(os.path.splitext(path)[0])
+    print(path)
+    print(os.path.splitext(path)[0])
+print(len(imgList))
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+print("Encoding Started")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown , studentIds]
+print(encodeListKnown)
+print("Encoding complete")
+
+file = open(r"D:\Higher studies\Projects\fr_demo\SaveFile.p",'wb')
+pickle.dump(encodeListKnownWithIds,file)
+file.close()
+print("File Saved")
